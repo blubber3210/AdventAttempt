ea750c139b2c18d960f752dbe1dac132f342215f
Elida <blubber3210@gmail.com>
2025-12-01 15:49:18 +0100

    Add project files.

---
+++ Advent/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Advent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter inputs");
+            string inputLine = Console.ReadLine();
+
+            string[] inputs = inputLine.Split(' ', StringSplitOptions.None);
+
+            int dial = 50;
+
+            SafeInput input = new SafeInput()
+
+
+        }
+    }
+}
---
+++ Advent/SafeInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent
+{
+    internal class SafeInput
+    {
+        public SafeInput(char letter, int number)
+        {
+            Letter = letter;
+            Number = number;
+        }
+
+        public char Letter { get; set; }
+        public int Number { get; set; }
+    }
+}
