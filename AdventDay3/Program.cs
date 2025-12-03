125cdb8f5fd489abb3787a33ff428b8f27ca76f2
Elida <blubber3210@gmail.com>
2025-12-03 20:36:06 +0100

    bu

---
+++ AdventDay3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
