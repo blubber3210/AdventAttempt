047a7776756d22f17de37573dbaa28476dda8cfd
Elida <blubber3210@gmail.com>
2025-12-03 10:33:33 +0100

    copy

---
+++ Advent2/SafeInput - Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2
+{
+    public class SafeInput
+    {
+        public SafeInput(char letter, int number)
+        {
+            Letter = letter;
+            Number = number;
+        }
+
+        public char Letter { get; set; }
+        public int Number { get; set; }
+        
+    
+    }
+}
