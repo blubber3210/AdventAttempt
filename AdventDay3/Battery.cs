454b5dfd07b220e017047fc7797f0aec3ed9ddfb
Elida <blubber3210@gmail.com>
2025-12-04 11:39:30 +0100

    backup men dag 3 er feil

---
+++ AdventDay3/Battery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventDay3
+{
+    internal class Battery
+    {
+        private int[] BatteryInts;
+
+        public Battery(int[] batteryInts)
+        {
+            BatteryInts = batteryInts;
+        }
+
+        public void FindHighestVoltage()
+        {
+            
+        }
+    }
+}
